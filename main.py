06dad8cf04a02b6e2a1428ce7e70a0ecea2218dd
Vanshrichardiya <143334266+Vanshrichardiya@users.noreply.github.com>
2025-08-21 02:25:08 +0530

    python file for task (d)

--- main.py
+++ main.py
@@ -1,1 +1,30 @@
-print("Hello World")
+def substract(a,b):
+    return a-b
+
+def add(a,b):
+    return a+b
+
+def multiply(a,b):
+    return a*b
+
+def divide(a,b):
+    if(b==0):
+        return "Error"
+    return a/b
+
+a=10
+b=5
+
+print("Substraction: ")
+print(substract(a,b))
+print()
+print("Addition: ")
+print(add(a,b))
+print()
+print("multiplication: ")
+print(multiply(a,b))
+print()
+print("Division: ")
+print(divide(a,b))
+print()
+
